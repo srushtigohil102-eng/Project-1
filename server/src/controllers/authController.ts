226a6d61064862860f4080b91e6c3accda304754
Karthika Devi <karthikadevi870@gmail.com>
2026-06-11 19:59:28 +0530

    Added login success message

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -42,6 +42,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     );
 
     res.status(200).json({
+      message: "Login successful",
       token,
       user: {
         id: user.id,
