77d6e847b42326985b991c086047e07c9de58362
Srushti <srushtigohil102@gmail.com>
2026-06-11 15:38:58 +0530

    chore: remove server folder from frontend branch

--- server/src/routes/employeeRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getEmployees } from "../controllers/employeeController";
-import { authenticate, requireRole } from "../middleware/authMiddleware";
-
-const router = express.Router();
-
-router.get("/", authenticate, requireRole("hr_manager"), getEmployees);
-
-export default router;
