52ac0e7615e999611765cebb453194ef27356eb1
srushtigohil102-eng <srushtigohil102@gmail.com>
2026-06-12 11:29:16 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,9 +17,11 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/employees", employeeRoutes);
 
-app.get("/", (_req, res) => {
-  res.send("Backend Running");
-});
+import { Request, Response } from 'express'
+
+app.get('/health', (_req: Request, res: Response) => {
+  res.json({ status: 'ok' })
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
